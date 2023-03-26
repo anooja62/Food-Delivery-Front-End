c52a32bbe1b9a5cb50cd13bc1772ede5fb405cd4
anooja62 <anooja7777@gmail.com>
2023-03-26 09:33:32 +0530

    updated

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,12 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL : 'http://localhost:9000',
-})
+    baseURL: 'https://deliorder-api.onrender.com/',
+    headers: {
+      'Content-Type': 'application/json',
+      
+    },
+  });
 
 
 export default  instance
