e77fa9c73bdeb23dedb9d1f6094fd0b7cdb56f29
anooja62 <anooja7777@gmail.com>
2023-03-28 19:43:40 +0530

    all cookies updated

--- src/Pages/RestaurantLogin.jsx
+++ src/Pages/RestaurantLogin.jsx
@@ -30,15 +30,15 @@ const RestaurantLogin = () => {
       console.log(response);
       if (response.status === 200) {
         setCookie("restaurantdummy", "hyyy", { secure: true, sameSite: "none" });
-        setCookie("restaurantId", response.data._id, { secure: true, sameSite: "none" });
-        setCookie("restaurantName", response.data.name, { secure: true, sameSite: "none" });
-        setCookie("restaurantEmail", response.data.email, { secure: true, sameSite: "none" });
-        setCookie("restaurantPhone", response.data.phone, { secure: true, sameSite: "none" });
-        setCookie("restaurantLicense", response.data.license, { secure: true, sameSite: "none" });
-        setCookie("restaurantimgUrl", response.data.imgUrl, { secure: true, sameSite: "none" });
-        setCookie("restaurantAbout", response.data.about, { secure: true, sameSite: "none" });
-        setCookie("restaurantOwnername", response.data.ownername, { secure: true, sameSite: "none" });
-        setCookie("restaurantOwnerphone", response.data.ownerphone, { secure: true, sameSite: "none" });
+        setCookie("restaurantid", response.data._id, { secure: true, sameSite: "none" });
+        setCookie("restaurantname", response.data.name, { secure: true, sameSite: "none" });
+        setCookie("restaurantemail", response.data.email, { secure: true, sameSite: "none" });
+        setCookie("restaurantphone", response.data.phone, { secure: true, sameSite: "none" });
+        setCookie("restaurantlicense", response.data.license, { secure: true, sameSite: "none" });
+        setCookie("restaurantimgurl", response.data.imgUrl, { secure: true, sameSite: "none" });
+        setCookie("restaurantabout", response.data.about, { secure: true, sameSite: "none" });
+        setCookie("restaurantownername", response.data.ownername, { secure: true, sameSite: "none" });
+        setCookie("restaurantownerphone", response.data.ownerphone, { secure: true, sameSite: "none" });
         navigate("/admin-res");
        
       }
