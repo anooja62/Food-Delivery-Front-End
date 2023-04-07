9038b9060d5a3da76c1487ea65e407087c457005
anooja62 <anooja7777@gmail.com>
2023-04-07 08:56:11 +0530

    layout updated

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,7 +3,6 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routes from "../../routes/Routers";
 import Carts from "../UI/cart/Carts";
-
 import { useSelector } from "react-redux";
 
 function Layout() {
@@ -19,7 +18,7 @@ function Layout() {
       {showCart && <Carts />}
 
       <div>
-        <Routes />
+        {isAdminPage ? <Routes /> : null}
       </div>
 
       {!isAdminPage && <Footer />}
